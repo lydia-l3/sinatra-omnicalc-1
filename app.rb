c314c86261caea5486b7bce5e3c11dedeb9fceb1
lydia-l3 <122927604+lydia-l3@users.noreply.github.com>
2024-04-16 20:24:27 +0000

    all omnicalc 1 pages complete final commit

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'active_support/all'
 
 get("/howdy") do
   erb(:hello)
@@ -37,4 +38,42 @@ get("/") do
   "
   <h1>howdy</h1>
   "
+end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get("/payment/results") do
+  @apr_input = params.fetch("user_apr").to_f
+  @apr_final = @apr_input.to_fs(:percentage, {:precision => 4})
+
+  @years_final = params.fetch("user_years").to_i
+
+  @principal_input = params.fetch("user_pv").to_f
+  @principal_final = @principal_input.to_fs(:currency)
+
+  @r = (@apr_input.to_f / 100) /12
+  @n = @years_final.to_i * 12
+  @pv = @principal_input
+  @numerator = @r*(@pv)
+  @denominator = 1-(1+@r)**-@n
+
+  @payment_calc = @numerator/@denominator
+  @payment_result = @payment_calc.to_fs(:currency)
+
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:new_random_calc)
+end
+
+get("/random/results") do
+  @minimum = params.fetch("user_min").to_f
+  @maximum = params.fetch("user_max").to_f
+
+  @random_result = rand(@minimum...@maximum)
+
+  erb(:random_results)
 end
