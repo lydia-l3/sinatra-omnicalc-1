eaf6737f553e20d5a9a48f183f2a9039c40b4d8f
lydia-l3 <122927604+lydia-l3@users.noreply.github.com>
2024-04-16 06:07:41 +0000

    square with form and sqrt with form work correctly

--- app.rb
+++ app.rb
@@ -1,9 +1,40 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num_two = params.fetch("users_number").to_f
+
+  @the_result_two = Math.sqrt(@the_num_two)
+
+  erb(:square_root_results)
+end
+
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>howdy</h1>
   "
 end
